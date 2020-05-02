568fa8d2f5f4a3764be096a1dfe80a6f9a97115e
Divya Rachel <divyadeepti2000@gmail.com>
2020-05-02 20:32:38 +0530

    Testing commit 1

---
+++ first.py
@@ -0,0 +1,32 @@
+from flask import Flask, url_for
+from markupsafe import escape
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    
+    return render_template('login.html', error=error)
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
